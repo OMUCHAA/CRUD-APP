6924d449e7137d4bee7aa293abe46a99e6861b56
OMUCHAA <shemomu@gmail.com>
2026-06-20 02:04:47 +0300

    changing the bg-color of the entire page to dark mode, and creating the navgation buttons.

--- frontend/src/components/MainLayout.vue
+++ frontend/src/components/MainLayout.vue
@@ -1,8 +1,16 @@
 <template>
-    <h1>CRUD APP</h1>
-    <slot/>
+  <div class="text-white flex flex-col w-full items-center">
+    <h1 class="text-3xl font-bold mt-5">WELCOME</h1>
+    <div class="mt-5">
+      <router-link to="/create" class="bg-green-600 font-semibold mr-3 rounded py-2 px-4">
+        Add Contact
+      </router-link>
+      <router-link to="/" class="bg-blue-800 font-semibold mr-3 rounded py-2 px-4"
+        >Contact</router-link
+      >
+    </div>
+  </div>
+  <slot />
 </template>
 
-<script setup>
-
-</script>
+<script setup></script>

bcb99668e109488bc363e140a34d0af6655dce84
OMUCHAA <shemomu@gmail.com>
2026-06-28 10:57:54 +0300

    Creating function that saves user information to the databse, also changing the router file createwebhashistory to just createwebhistory.

--- frontend/src/components/pages/CreatePage.vue
+++ frontend/src/components/pages/CreatePage.vue
@@ -2,16 +2,20 @@
   <MainLayout>
     <div class="flex flex-col items-center mt-5">
       <h1 class="text-3xl font-semibold text-white">Add New User</h1>
+      <p class="text-white bg-green-600 text-sm rounded py-1 px-2" v-if="successMessage">
+        {{ successMessage }}
+      </p>
     </div>
     <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-3 lg:px-8">
       <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form class="space-y-6" action="#" method="POST">
+        <form class="space-y-6" @submit.prevent="submit">
           <div>
             <label for="name" class="block text-sm/6 font-medium text-gray-100"
               >Name</label
             >
             <div class="mt-2">
               <input
+                v-model="userData.name"
                 type="text"
                 name="name"
                 id="name"
@@ -26,6 +30,7 @@
             >
             <div class="mt-2">
               <input
+                v-model="userData.email"
                 type="email"
                 name="email"
                 id="email"
@@ -40,6 +45,7 @@
             >
             <div class="mt-2">
               <input
+                v-model="userData.address"
                 type="text"
                 name="address"
                 id="address"
@@ -57,6 +63,7 @@
             </div>
             <div class="mt-2">
               <input
+                v-model="userData.phone"
                 type="text"
                 name="phone"
                 id="phone"
@@ -80,5 +87,30 @@
   </MainLayout>
 </template>
 <script setup>
+import axiosClient from "../../axios.js";
+import router from "../../router.js";
 import MainLayout from "../MainLayout.vue";
+import { ref } from "vue";
+
+const userData = ref({
+  name: "",
+  email: "",
+  address: "",
+  phone: "",
+});
+
+const successMessage = ref(false);
+
+async function submit() {
+  try {
+    const response = await axiosClient.post("/api/details", userData.value);
+    console.log(response.data);
+    successMessage.value = response.data.message;
+    setTimeout(() => {
+      router.push("/");
+    }, 2000);
+  } catch (error) {
+    console.log(error.response.message);
+  }
+}
 </script>
